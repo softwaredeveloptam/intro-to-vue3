f638ad2b4aaee11f9c540e603033ef77b1dbde1b
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-10-11 03:41:28 +0900

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A pair of socks that warm your feet.'
+        }
+    }
+})
